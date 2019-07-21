432c797ff9183e9f47722525a200ce20082d46e8
Jesse James Burton <jesse@burtonmediainc.com>
2019-07-21 07:58:05 -0600

    Adding new thumbnail code

--- layout.js
+++ layout.js
@@ -1,20 +1,48 @@
-const slides = [
-    'images/StrangemorePHOTO-8299.jpg',
-    'images/StrangemorePHOTO-8304.jpg',
-    'images/StrangemorePHOTO-8306.jpg',
-    'images/StrangemorePHOTO-8340.jpg',
-    'images/StrangemorePHOTO-8155.jpg',
-    'images/StrangemorePHOTO-8151.jpg',
-    'images/StrangemorePHOTO-8120.jpg'
+const arr = [
+  'images/StrangemorePHOTO-8299.jpg',
+  'images/StrangemorePHOTO-8304.jpg',
+  'images/StrangemorePHOTO-8306.jpg',
+  'images/StrangemorePHOTO-8120.jpg',
+  'images/StrangemorePHOTO-8151.jpg',
+  'images/StrangemorePHOTO-8155.jpg',
+  'images/StrangemorePHOTO-8340.jpg'
 ]
 
-const gallery = document.querySelector('.thumbnails-list');
-const thumbnail = document.querySelector('.thumbnail.active');
-console.log(thumbnail);
-console.log(thumbnail.offsetWidth);
+var dir = "left";
 
-
-function moveRight(){
-    var mv = gallery.style.right  + thumbnail.offsetWidth;
-    gallery.style.right = `${mv}px`;
+const shiftRight = () => {
+  const x = arr.shift();
+  arr.push(x);
+  dir = "right";
+  populateImages(arr);
 }
+
+const shiftLeft = () => {
+  const x = arr.pop();
+  arr.unshift(x);
+  dir = "left";
+  populateImages(arr);
+}
+
+const populateImages = (images) => {
+
+  const thumbnails = document.querySelectorAll(".thumbnail");
+
+  thumbnails.forEach((thumbnail, index) => {
+    thumbnail.innerHTML = "";
+    thumbnail.append(createImage(images[index], dir));
+  })
+
+}
+
+const createImage = (src, className) => {
+
+  const img = document.createElement("img");
+  img.classList.add(className);
+  img.src = src;
+  return img;
+
+}
+
+populateImages(arr);
+
